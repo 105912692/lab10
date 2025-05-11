6803bd0aae2116fc3af4e3071328af2664a6c769
Oliver Scott <105912692@student.swin.edu.au>
2025-05-11 21:25:16 +1000

    login.php variable error name change

--- login.php
+++ login.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = mysqli_real_escape_string($conn, trim($_POST['username']));
     $password = mysqli_real_escape_string($conn, trim($_POST['password']));
 
-    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+    $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $query);
     $user = mysqli_fetch_assoc($result);
 
