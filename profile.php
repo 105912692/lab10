9e57618b93b0055d5e7cfd45d51ec29ff33dcc69
Oliver Scott <105912692@student.swin.edu.au>
2025-05-11 21:42:03 +1000

    edit profile start progress test

--- profile.php
+++ profile.php
@@ -8,3 +8,7 @@ if (!isset($_SESSION['username']) || !isset($_SESSION['password'])) {
 ?>
 
 <h1>Welcome, <?php echo htmlspecialchars($_SESSION['username']); ?> with password: <?php echo htmlspecialchars($_SESSION['password']); ?></h1>
+
+<h1?>Edit profile:</h1>
+<br>
+<h2>Test</h2>
